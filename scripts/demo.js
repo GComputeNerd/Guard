eb9c3a0c1c24b945d58acaf344dcaa155d320b48
GComputeNerd <GComputeNerd@users.noreply.github.com>
2024-05-15 22:16:43 +0800

    using chrome.storage for livedetection field

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,1 +1,7 @@
-console.log('Extension is Running');
+chrome.storage.sync.get('live_detection', function(item) {
+    live_det = item.live_detection;
+
+    if (live_det == 1) {
+        alert("Live Detection")
+    }
+});
