5370867073c3c893bcb0fe7052ac7852526fd364
GComputeNerd <GComputeNerd@users.noreply.github.com>
2024-05-15 12:18:24 +0800

    Added slider animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,32 @@
 console.log('Extension is running');
 
 scan_btn = document.querySelector(".scanButton");
+live_det = false;
+
+toggleMain = document.querySelector(".toggle");
+toggleElements = document.querySelectorAll(".toggle *");
+
+offColor = "#546E7A"
+onColor = "#42A5F5"
 
 function test() {
     alert("Hi");
 }
 
-scan_btn.onclick = test;
+function toggle() {
+    if (!live_det) {
+        for (et of toggleElements) {
+            et.style.transform = "translateX(2em)";
+            toggleMain.style.backgroundColor = onColor
+        }
+    } else {
+        for (et of toggleElements) {
+            et.style.transform = "translateX(-0.5em)";
+            toggleMain.style.backgroundColor = offColor
+        }
+    }
+    live_det = !live_det;
+}
+
+scan_btn.onclick = test;   
+toggleMain.onclick = toggle;
